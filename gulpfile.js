21325db3c9ad59105477667831d88b93738c95ef
bebeto123 <bebeto123@live.ru>
2021-04-08 23:19:09 +0300

    complete good gulp

--- gulpfile.js
+++ gulpfile.js
@@ -10,17 +10,20 @@ const imagemin = require('gulp-imagemin')
 const newer = require('gulp-newer')
 const del = require('del')
 const pug = require('gulp-pug')
+const pugLinter = require('gulp-pug-linter')
 
-
+const eslint = require('gulp-eslint')
+const babel = require('gulp-babel')
+const terser = require('gulp-terser')
+const sourcemaps = require('gulp-sourcemaps')
 
 function pug2html() {
-  return src('src/pages/*.pug').pipe(pug()).pipe(dest('src/'))
+  return src('src/pages/*.pug')
+    .pipe(pugLinter({ reporter: 'default', failAfterError: true }))
+    .pipe(pug())
+    .pipe(dest('src/'))
+  // failAfterError остановит сборку
 }
-
-
-
-
-
 
 function browsersync() {
   browserSync.init({
@@ -31,12 +34,12 @@ function browsersync() {
 }
 
 function styles() {
-  return src('src/style/main.less') // .styl
+  return src('src/styles/main.less') // .styl
     .pipe(less()) // .pipe(stylus())
     .pipe(concat('app.min.css'))
     .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
     .pipe(cleancss({ level: { 1: { specialComments: 0 } } /* format: 'beautify' */ }))
-    .pipe(dest('src/style/'))
+    .pipe(dest('src/styles/'))
     .pipe(browserSync.stream())
 }
 
@@ -52,7 +55,16 @@ function cleanimg() {
 }
 
 function scripts() {
-  return src(['src/js/jquery-3.5.1.min.js', 'src/js/main.js'])
+  // return src(['src/js/jquery-3.5.1.min.js', 'src/js/main.js'])
+  return src('src/js/main.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(terser())
+    .pipe(sourcemaps.write())
     .pipe(concat('app.min.js'))
     .pipe(uglify())
     .pipe(dest('src/js/'))
@@ -60,9 +72,10 @@ function scripts() {
 }
 
 function startWatch() {
-  watch('src/style/**/*.less', styles) // 'app/style/**/*.styl'
+  watch('src/styles/**/*.less', styles) // 'app/style/**/*.styl'
   watch(['src/**/*.js', '!src/**/*.min.js'], scripts)
-  watch('src/**/*.html').on('change', browserSync.reload)
+  // watch('src/**/*.html').on('change', browserSync.reload)
+  watch('src/**/*.pug', pug2html).on('change', browserSync.reload)
   watch('src/assets/src/**/*', images)
 }
 
@@ -74,7 +87,7 @@ function buildcopy() {
 }
 
 function cleandist() {
-	return del('build/**/*', { force: true })
+  return del('build/**/*', { force: true })
 }
 
 exports.browsersync = browsersync
@@ -85,5 +98,5 @@ exports.scripts = scripts
 exports.buildcopy = buildcopy
 exports.pug2html = pug2html
 
-exports.default = parallel(styles, scripts, browsersync, startWatch)
+exports.default = parallel(pug2html, styles, scripts, browsersync, startWatch)
 exports.build = series(cleandist, styles, scripts, images, buildcopy)
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
+// $('.page__header').on('click', function () {
+//   $(this).toggleClass('active')
+//   // $(this).toggleClass('puppa')
+// })
 
-$('.page__header').on('click', function() {
-  $(this).toggleClass('active')
-  // $(this).toggleClass('puppa')
+const header = document.querySelector('.page__header')
+header.addEventListener('click', function (e) {
+  console.log(e.currentTarget)
+
+  this.classList.toggle('active')
 })
